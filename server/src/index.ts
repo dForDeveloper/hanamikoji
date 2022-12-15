644e7087a9f28d9f9cb234496de47481cc833532
Jeo D <jeo@dfor.dev>
2022-12-14 23:20:46 -0700

    Add Postgres database to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
 import { Server, Origins } from 'boardgame.io/server';
+import { PostgresStore } from 'bgio-postgres';
 import { Hanamikoji } from 'game-logic';
+
+const db = new PostgresStore('postgresql://root:password@localhost:5432/postgres');
 
 const server = Server({
   games: [Hanamikoji],
   origins: [Origins.LOCALHOST_IN_DEVELOPMENT],
+  db,
 });
 
 server.run(8000);
