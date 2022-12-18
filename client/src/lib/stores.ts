e06a9cb32b9467c7255db0f37f5fd5c91eb29d73
Jeo D <jeo@dfor.dev>
2022-12-18 16:37:59 -0700

    Add env vars to sveltekit

--- client/src/lib/stores.ts
+++ client/src/lib/stores.ts
@@ -1,7 +1,8 @@
 import { LobbyClient } from 'boardgame.io/client';
+import { PUBLIC_BASE_URL } from '$env/static/public';
 import { readable } from 'svelte/store';
 import type { Readable } from 'svelte/store';
 
-const lobbyClient = new LobbyClient({ server: 'http://localhost:8000' });
+const lobbyClient = new LobbyClient({ server: PUBLIC_BASE_URL });
 
 export const lobby: Readable<LobbyClient> = readable(lobbyClient);
