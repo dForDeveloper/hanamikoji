8d21f222ecf4add3e556cd7cae89b08c09ba114d
Jeo D <e8gea6w5z8id@opayq.com>
2022-12-23 16:14:45 -0800

    Refactor home page to use NameForm component

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { getPlayerData, setPlayerData } from '$lib/utils';
+  import NameForm from '$lib/components/NameForm.svelte';
+  import { error } from '@sveltejs/kit';
   import { goto } from '$app/navigation';
-  import { lobby } from '$lib/stores.ts';
+  import { lobby } from '$lib/stores';
   import { onMount } from 'svelte';
   import type { Player } from '$lib/types';
 
@@ -11,34 +13,21 @@
     player = getPlayerData();
   });
 
-  async function goToNewMatch(): Promise<void> {
-    console.log('player in goToNewMatch', player);
-    player = setPlayerData({ name: player.name, credentials: '' });
-    const matchID = await createMatch();
-    goto(`match/${matchID}`);
+  async function handleClick(event: CustomEvent): Promise<void> {
+    try {
+      setPlayerData({ name: event.detail.name, credentials: '' });
+      const { matchID } = await $lobby.createMatch('hanamikoji', { numPlayers: 2 });
+      goto(`match/${matchID}`);
+    } catch (err) {
+      throw error(500, { message: 'Error creating match' });
+    }
   }
 
-  async function createMatch(): Promise<string> {
-    try {
-      const res = await $lobby.createMatch('hanamikoji', { numPlayers: 2 });
-      return res.matchID;
-    } catch (error) {
-      throw new Error('Error creating match');
-    }
+  function getIsDisabled(name: string): boolean {
+    return !name;
   }
 </script>
 
 <main class="grid place-items-center h-screen">
-  <form class="grid grid-rows-3 gap-2 w-64">
-    <label for="nickname">Choose a nickname</label>
-    <input
-      id="nickname"
-      type="text"
-      name="nickname"
-      placeholder="SpongeBob"
-      class="border-2 w-full"
-      bind:value={player.name}
-    />
-    <button class="border-2 w-full hover:cursor-pointer" on:click={goToNewMatch} disabled={!player.name}>New Match</button>
-  </form>
+  <NameForm on:clickEvent={handleClick} getIsDisabled={getIsDisabled} name={player.name} />
 </main>
