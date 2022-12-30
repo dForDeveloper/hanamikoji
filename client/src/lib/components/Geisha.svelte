49eaadf3eed0d43fdbe2ed74a8670e46a7f11324
Jeo D <jeo@dfor.dev>
2022-12-29 23:36:31 -0700

    Add Geisha Card images

---
+++ client/src/lib/components/Geisha.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Color } from "game-logic";
+
+  export let color: Color;
+
+  const colorToSource = {
+    [Color.PINK]: '/images/pink-geisha.webp',
+    [Color.GREEN]: '/images/green-geisha.webp',
+    [Color.ORANGE]: '/images/orange-geisha.webp',
+    [Color.BLUE]: '/images/blue-geisha.webp',
+    [Color.YELLOW]: '/images/yellow-geisha.webp',
+    [Color.RED]: '/images/red-geisha.webp',
+    [Color.PURPLE]: '/images/purple-geisha.webp',
+  };
+</script>
+
+<img src={colorToSource[color]} alt={`${color} geisha card`} />
