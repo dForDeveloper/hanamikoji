802200ddb51b0be4a638d02c538bdf4e3b641a38
Jeo D <jeo@dfor.dev>
2023-01-07 17:38:13 -0700

    Remove grayscale effect from deck

--- client/src/lib/components/Deck.svelte
+++ client/src/lib/components/Deck.svelte
@@ -3,24 +3,22 @@
 
   export let handleClick: () => void;
   export let isDisabled: boolean;
-
-  const sharedClasses = isDisabled ? 'absolute w-full grayscale' : 'absolute w-full';
 </script>
 
 <div class="relative aspect-[8/11] rounded-xl">
-  <button on:click={handleClick} class={`${sharedClasses} z-50`} disabled={isDisabled}>
+  <button on:click={handleClick} class={'absolute w-full z-50'} disabled={isDisabled}>
     <Card type="back" isHoverable={!isDisabled} />
   </button>
-  <div class={`${sharedClasses} z-40 top-1 scale-x-[0.99]`}>
+  <div class={'absolute w-full z-40 top-1 scale-x-[0.99]'}>
     <Card type="back" />
   </div>
-  <div class={`${sharedClasses} z-30 top-2 scale-x-[0.98]`}>
+  <div class={'absolute w-full z-30 top-2 scale-x-[0.98]'}>
     <Card type="back" />
   </div>
-  <div class={`${sharedClasses} z-20 top-3 scale-x-[0.97]`}>
+  <div class={'absolute w-full z-20 top-3 scale-x-[0.97]'}>
     <Card type="back" />
   </div>
-  <div class={`${sharedClasses} z-10 top-4 scale-x-[0.96]`}>
+  <div class={'absolute w-full z-10 top-4 scale-x-[0.96]'}>
     <Card type="back" />
   </div>
 </div>
