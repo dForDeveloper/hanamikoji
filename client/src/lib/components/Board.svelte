40fd0531e741a033dbc2d24dc7987bd46c8e3669
Jeo D <jeo@dfor.dev>
2023-01-10 21:25:57 -0700

    Move more logic into SelectedCardArea

--- client/src/lib/components/Board.svelte
+++ client/src/lib/components/Board.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
-  import AcknowledgeReveal from '$lib/components/AcknowledgeReveal.svelte';
-  import AcknowledgeChoice from '$lib/components/AcknowledgeChoice.svelte';
   import Actions from '$lib/components/Actions.svelte';
   import Card from '$lib/components/Card.svelte';
   import CardStack from '$lib/components/CardStack.svelte';
   import Hand from '$lib/components/Hand.svelte';
   import Opponent from '$lib/components/Opponent.svelte';
   import SelectedCardArea from '$lib/components/SelectedCardArea.svelte';
-  import { getInstructions } from '$lib/instruction-messages';
   import { onDestroy } from 'svelte';
-  import type { Action, GameState, GeishaCard, ItemCard, Player } from 'game-logic';
+  import type { GameState, GeishaCard, Player } from 'game-logic';
   import type { Ctx } from 'boardgame.io';
   import type { SelectedCard } from '$lib/types';
 
@@ -22,8 +19,7 @@
   let opponentStage: string;
   let currentAction: string;
   let selectedCards: SelectedCard[] = [null, null, null, null];
-  let presentedSelection = '';
-  let opponentChoice = '';
+  let selectedPresentedIndex = '';
 
   const unsubscribe = client.subscribe((gameState: { G: GameState; ctx: Ctx }) => {
     if (gameState && gameState.G && gameState.ctx) {
@@ -32,9 +28,8 @@
       currentAction = G.currentAction ? G.currentAction : '';
       playerStage = ctx.activePlayers ? ctx.activePlayers[playerID] : '';
       opponentStage = ctx.activePlayers ? ctx.activePlayers[opponentPlayerID] : '';
-      opponentChoice = G.opponentChoice;
       setSelectedCardsFromHand([null, null, null, null]);
-      presentedSelection = '';
+      setSelectedPresentedIndex('');
     } else {
       client.getState();
     }
@@ -55,16 +50,6 @@
     return G.players[id];
   }
 
-  function getPresentedCards(G: GameState, currentAction: string): ItemCard[] {
-    if (currentAction === '2') {
-      return G.presentedCards;
-    } else if (currentAction === '3') {
-      return G.presentedPairs.flat();
-    } else {
-      throw new Error('Presented cards should not exist');
-    }
-  }
-
   function drawCard(): void {
     client.moves.draw();
   }
@@ -76,6 +61,14 @@
   function undoAction(): void {
     setSelectedCardsFromHand([null, null, null, null]);
     client.undo();
+  }
+
+  function selectCardsAsCurrent(selectedIndexes: string[]): void {
+    client.moves.selectCardsAsCurrent(selectedIndexes);
+  }
+
+  function selectCardsAsOpposing(selectedIndex: string): void {
+    client.moves.selectCardsAsOpposing(selectedIndex);
   }
 
   function acknowledgeChoice(): void {
@@ -94,110 +87,34 @@
     selectedCards = updatedSelectedCards;
   }
 
-  function setPresentedSelection(updatedPresentedSelection: string): void {
-    presentedSelection = updatedPresentedSelection;
+  function setSelectedPresentedIndex(updatedSelectedPresentedIndex: string): void {
+    selectedPresentedIndex = updatedSelectedPresentedIndex;
   }
 
-  function confirmSelection(selectedCards: SelectedCard[], presentedSelection: string): void {
-    if (playerStage === 'selectCardsAsCurrentPlayer') {
-      const selectedCardIndexes = selectedCards.filter((maybeCard) => maybeCard !== null).map((itemCard) => itemCard!.index.toString());
-      client.moves.selectCardsAsCurrent(selectedCardIndexes);
-    } else if (playerStage === 'selectCardsAsOpposingPlayer') {
-      client.moves.selectCardsAsOpposing(presentedSelection);
-    }
-  }
 
-  function getIsConfirmationButtonDisabled(selectedCards: SelectedCard[], presentedSelection: string): boolean {
-    if (playerStage === 'selectCardsAsCurrentPlayer') {
-      const requiredSelectedCardCount = Number(currentAction) + 1;
-      const nonNullSelectedCardCount = selectedCards.filter((maybeCard) => maybeCard !== null).length;
-      return nonNullSelectedCardCount !== requiredSelectedCardCount;
-    } else if (playerStage === 'selectCardsAsOpposingPlayer') {
-      return presentedSelection === '';
-    } else {
-      throw new Error('Confirmation button should exist');
-    }
-  }
-
-  function getIsActionUndoable(G: GameState, id: string): boolean {
-    const player = getPlayer(G, id);
-    return Object.values(player.actions).some((action: Action) => action.enabled);
-  }
-
-  function getRevealedCard(G: GameState, playerStage: string, opponentStage: string): ItemCard {
-    let player;
-    if (playerStage === 'acknowledgeReveal') {
-      player = getPlayer(G, opponentPlayerID);
-    } else if (opponentStage === 'acknowledgeReveal') {
-      player = getPlayer(G, playerID);
-    }
-
-    if (player && player.actions[0].savedCard) {
-      return player.actions[0].savedCard;
-    } else {
-      throw new Error('No hidden card to reveal');
-    }
-  }
 </script>
 
 {#if G && ctx}
   <main class="grid grid-cols-[2fr_3fr] grid-rows-[1fr_4fr_1fr] gap-2 h-screen p-2 font-nunito">
     <Opponent player={getPlayer(G, opponentPlayerID)} />
-    {#if playerStage === 'acknowledgeOpponentChoice' || opponentStage === 'acknowledgeOpponentChoice'}
-      <AcknowledgeChoice
-        presentedCards={getPresentedCards(G, currentAction)}
-        {playerStage}
-        {opponentStage}
-        {currentAction}
-        {opponentChoice}
-        {acknowledgeChoice}
-      />
-    {:else if playerStage === 'acknowledgeReveal' || opponentStage === 'acknowledgeReveal'}
-      <AcknowledgeReveal
-        revealedCard={getRevealedCard(G, playerStage, opponentStage)}
-        {playerStage}
-        {opponentStage}
-        {currentAction}
-        {acknowledgeReveal}
-      />
-    {:else}
-      <section aria-label="game-interface" class="grid grid-rows-[1fr_16.2vh_1fr]">
-        <div aria-label="instruction" class="place-self-center max-w-prose h-full">
-          {#each getInstructions(currentAction, playerStage, opponentStage) as instruction}
-            <p class="text-3xl my-6">{instruction}</p>
-          {/each}
-        </div>
-        <SelectedCardArea
-          presentedCards={getPresentedCards(G, currentAction)}
-          {playerStage}
-          {opponentStage}
-          {currentAction}
-          {presentedSelection}
-          {selectedCards}
-          {drawCard}
-          {setPresentedSelection}
-        />
-        <div class="flex pt-10 justify-center gap-8">
-          {#if playerStage === 'selectCardsAsCurrentPlayer' && getIsActionUndoable(G, playerID)}
-            <button
-              on:click={() => undoAction()}
-              class="bg-gray-600 text-white text-xl h-14 w-32 rounded-full shadow-sm shadow-gray-600 hover:shadow hover:shadow-gray-600"
-            >
-              undo
-            </button>
-          {/if}
-          {#if playerStage === 'selectCardsAsCurrentPlayer' || playerStage === 'selectCardsAsOpposingPlayer'}
-            <button
-              on:click={() => confirmSelection(selectedCards, presentedSelection)}
-              disabled={getIsConfirmationButtonDisabled(selectedCards, presentedSelection)}
-              class="bg-violet-300 text-xl h-14 w-32 rounded-full shadow-sm shadow-gray-600 hover:shadow hover:shadow-gray-600 disabled:bg-gray-200 disabled:shadow-none"
-            >
-              confirm
-            </button>
-          {/if}
-        </div>
-      </section>
-    {/if}
+    <SelectedCardArea
+      {G}
+      {playerID}
+      {opponentPlayerID}
+      {playerStage}
+      {opponentStage}
+      {currentAction}
+      {selectedCards}
+      {selectedPresentedIndex}
+      {getPlayer}
+      {drawCard}
+      {undoAction}
+      {selectCardsAsCurrent}
+      {selectCardsAsOpposing}
+      {acknowledgeChoice}
+      {acknowledgeReveal}
+      {setSelectedPresentedIndex}
+    />
     <section aria-label="game-board" class="grid grid-rows-[1fr_20vh_1fr]">
       <div aria-label="opponent-played-cards" class="flex flex-row justify-center space-x-4">
         {#each getGeishaCards(G) as geishaCard}
--- client/src/lib/components/SelectedCardArea.svelte
+++ client/src/lib/components/SelectedCardArea.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
+  import AcknowledgeChoice from '$lib/components/AcknowledgeChoice.svelte';
+  import AcknowledgeReveal from '$lib/components/AcknowledgeReveal.svelte';
   import Card from '$lib/components/Card.svelte';
   import Deck from '$lib/components/Deck.svelte';
-  import type { ItemCard } from 'game-logic';
+  import { getInstructions } from '$lib/instruction-messages';
+  import type { Action, GameState, ItemCard, Player } from 'game-logic';
   import type { SelectedCard } from '$lib/types';
 
+  export let G: GameState;
+  export let playerID: string;
+  export let opponentPlayerID: string;
   export let playerStage: string;
   export let opponentStage: string;
   export let currentAction: string;
-  export let presentedSelection: string;
   export let selectedCards: SelectedCard[];
-  export let presentedCards: ItemCard[];
+  export let selectedPresentedIndex: string;
+  export let getPlayer: (G: GameState, id: string) => Player;
   export let drawCard: () => void;
-  export let setPresentedSelection: (updatedPresentedSelection: string) => void;
+  export let undoAction: () => void;
+  export let selectCardsAsCurrent: (selectedIndexes: string[]) => void;
+  export let selectCardsAsOpposing: (selectedIndex: string) => void;
+  export let acknowledgeChoice: () => void;
+  export let acknowledgeReveal: () => void;
+  export let setSelectedPresentedIndex: (updatedPresentedSelection: string) => void;
 
   function getSelectedCardsToDisplay(currentAction: string, selectedCards: SelectedCard[]): SelectedCard[] {
     return selectedCards.slice(0, Number(currentAction) + 1);
@@ -19,98 +30,199 @@
 
   function selectFromPresented(currentAction: string, i: number): void {
     if (currentAction === '2') {
-      setPresentedSelection(i.toString());
+      setSelectedPresentedIndex(i.toString());
     } else if (currentAction === '3') {
-      if (i <= 1) setPresentedSelection('0');
-      if (i >= 2) setPresentedSelection('1');
+      if (i <= 1) setSelectedPresentedIndex('0');
+      if (i >= 2) setSelectedPresentedIndex('1');
     }
   }
 
   function deselectFromPresented(): void {
-    setPresentedSelection('');
+    setSelectedPresentedIndex('');
+  }
+
+  function confirmSelection(selectedCards: SelectedCard[], selectedPresentedIndex: string): void {
+    if (playerStage === 'selectCardsAsCurrentPlayer') {
+      const selectedCardIndexes = selectedCards.filter((maybeCard) => maybeCard !== null).map((itemCard) => itemCard!.index.toString());
+      selectCardsAsCurrent(selectedCardIndexes);
+    } else if (playerStage === 'selectCardsAsOpposingPlayer') {
+      selectCardsAsOpposing(selectedPresentedIndex);
+    }
+  }
+
+  function getIsConfirmationButtonDisabled(selectedCards: SelectedCard[], selectedPresentedIndex: string): boolean {
+    if (playerStage === 'selectCardsAsCurrentPlayer') {
+      const requiredSelectedCardCount = Number(currentAction) + 1;
+      const nonNullSelectedCardCount = selectedCards.filter((maybeCard) => maybeCard !== null).length;
+      return nonNullSelectedCardCount !== requiredSelectedCardCount;
+    } else if (playerStage === 'selectCardsAsOpposingPlayer') {
+      return selectedPresentedIndex === '';
+    } else {
+      throw new Error('Confirmation button should not exist');
+    }
+  }
+
+  function getPresentedCards(G: GameState, currentAction: string): ItemCard[] {
+    if (currentAction === '2') {
+      return G.presentedCards;
+    } else if (currentAction === '3') {
+      return G.presentedPairs.flat();
+    } else {
+      throw new Error('Presented cards should not exist');
+    }
+  }
+
+  function getOpponentChoice(G: GameState): string {
+    return G.opponentChoice;
+  }
+
+  function getRevealedCard(G: GameState, playerStage: string, opponentStage: string): ItemCard {
+    let player;
+
+    if (playerStage === 'acknowledgeReveal') {
+      player = getPlayer(G, opponentPlayerID);
+    } else if (opponentStage === 'acknowledgeReveal') {
+      player = getPlayer(G, playerID);
+    }
+
+    if (player && player.actions[0].savedCard) {
+      return player.actions[0].savedCard;
+    } else {
+      throw new Error('No hidden card to reveal');
+    }
+  }
+
+  function getIsActionUndoable(G: GameState, id: string): boolean {
+    const player = getPlayer(G, id);
+    return Object.values(player.actions).some((action: Action) => action.enabled);
   }
 </script>
 
-<div aria-label="selected-card-area" class="flex flex-row justify-center space-x-2">
-  {#if playerStage}
-    {#if playerStage === 'draw'}
-      <Deck handleClick={() => drawCard()} isDisabled={false} />
-    {:else if playerStage === 'selectCardsAsCurrentPlayer'}
-      <div class="flex flex-row justify-center space-x-2">
-        {#each getSelectedCardsToDisplay(currentAction, selectedCards) as selectedCard, i}
-          {#if currentAction === '3' && i === 2}
-            <div class="h-[16.2vh] w-[11.53vh]" />
-          {/if}
-          <div class="h-[16.2vh] w-[11.53vh]">
-            {#if selectedCard}
-              <Card type="item" color={selectedCard.color} />
-            {:else}
-              <Card type="empty" />
-            {/if}
-          </div>
-        {/each}
-      </div>
-    {:else if playerStage === 'selectCardsAsOpposingPlayer'}
-      {#if currentAction === '2'}
-        <div class="flex flex-row justify-center space-x-2">
-          {#each presentedCards as card, i}
-            {#if presentedSelection === i.toString()}
-              <button on:click={() => deselectFromPresented()} class="h-[16.2vh] w-[11.53vh]">
-                <Card type="item" color={card.color} isSelected={true} isHoverable={true} isInverted={true} />
-              </button>
-            {:else}
-              <button on:click={() => selectFromPresented(currentAction, i)} class="h-[16.2vh] w-[11.53vh]">
-                <Card type="item" color={card.color} isSelected={false} isHoverable={true} />
-              </button>
-            {/if}
-          {/each}
-        </div>
-      {:else if currentAction === '3'}
-        <div class="flex flex-row justify-center space-x-2">
+{#if playerStage === 'acknowledgeOpponentChoice' || opponentStage === 'acknowledgeOpponentChoice'}
+  <AcknowledgeChoice
+    presentedCards={getPresentedCards(G, currentAction)}
+    opponentChoice={getOpponentChoice(G)}
+    {playerStage}
+    {opponentStage}
+    {currentAction}
+    {acknowledgeChoice}
+  />
+{:else if playerStage === 'acknowledgeReveal' || opponentStage === 'acknowledgeReveal'}
+  <AcknowledgeReveal
+    revealedCard={getRevealedCard(G, playerStage, opponentStage)}
+    {playerStage}
+    {opponentStage}
+    {currentAction}
+    {acknowledgeReveal}
+  />
+{:else}
+  <section aria-label="game-interface" class="grid grid-rows-[1fr_16.2vh_1fr]">
+    <div aria-label="instruction" class="place-self-center max-w-prose h-full">
+      {#each getInstructions(currentAction, playerStage, opponentStage) as instruction}
+        <p class="text-3xl my-6">{instruction}</p>
+      {/each}
+    </div>
+    <div aria-label="selected-card-area" class="flex flex-row justify-center space-x-2">
+      {#if playerStage}
+        {#if playerStage === 'draw'}
+          <Deck handleClick={() => drawCard()} isDisabled={false} />
+        {:else if playerStage === 'selectCardsAsCurrentPlayer'}
           <div class="flex flex-row justify-center space-x-2">
-            {#each presentedCards as card, i}
+            {#each getSelectedCardsToDisplay(currentAction, selectedCards) as selectedCard, i}
               {#if currentAction === '3' && i === 2}
                 <div class="h-[16.2vh] w-[11.53vh]" />
               {/if}
-              {#if (i <= 1 && presentedSelection === '0') || (i >= 2 && presentedSelection === '1')}
-                <button on:click={() => deselectFromPresented()} class="h-[16.2vh] w-[11.53vh]">
-                  <Card type="item" color={card.color} isSelected={true} isHoverable={true} isInverted={true} />
-                </button>
-              {:else}
-                <button on:click={() => selectFromPresented(currentAction, i)} class="h-[16.2vh] w-[11.53vh]">
-                  <Card type="item" color={card.color} isSelected={false} isHoverable={true} />
-                </button>
-              {/if}
+              <div class="h-[16.2vh] w-[11.53vh]">
+                {#if selectedCard}
+                  <Card type="item" color={selectedCard.color} />
+                {:else}
+                  <Card type="empty" />
+                {/if}
+              </div>
             {/each}
           </div>
-        </div>
-      {/if}
-    {/if}
-  {:else if opponentStage}
-    {#if opponentStage === 'draw'}
-      <Deck handleClick={() => false} isDisabled={true} />
-    {:else if opponentStage === 'selectCardsAsCurrentPlayer' || opponentStage === 'selectCardsAsOpposingPlayer'}
-      <div class="flex flex-row justify-center space-x-2">
-        {#if opponentStage === 'selectCardsAsCurrentPlayer'}
-          {#each Array(Number(currentAction) + 1) as _, i}
-            {#if currentAction === '3' && i === 2}
-              <div class="h-[16.2vh] w-[11.53vh]" />
-            {/if}
-            <div class="h-[16.2vh] w-[11.53vh]">
-              <Card type="empty" />
+        {:else if playerStage === 'selectCardsAsOpposingPlayer'}
+          {#if currentAction === '2'}
+            <div class="flex flex-row justify-center space-x-2">
+              {#each getPresentedCards(G, currentAction) as card, i}
+                {#if selectedPresentedIndex === i.toString()}
+                  <button on:click={() => deselectFromPresented()} class="h-[16.2vh] w-[11.53vh]">
+                    <Card type="item" color={card.color} isSelected={true} isHoverable={true} isInverted={true} />
+                  </button>
+                {:else}
+                  <button on:click={() => selectFromPresented(currentAction, i)} class="h-[16.2vh] w-[11.53vh]">
+                    <Card type="item" color={card.color} isSelected={false} isHoverable={true} />
+                  </button>
+                {/if}
+              {/each}
             </div>
-          {/each}
-        {:else if opponentStage === 'selectCardsAsOpposingPlayer'}
-          {#each presentedCards as card, i}
-            {#if currentAction === '3' && i === 2}
-              <div class="h-[16.2vh] w-[11.53vh]" />
-            {/if}
-            <div class="h-[16.2vh] w-[11.53vh]">
-              <Card type="item" color={card.color} />
+          {:else if currentAction === '3'}
+            <div class="flex flex-row justify-center space-x-2">
+              <div class="flex flex-row justify-center space-x-2">
+                {#each getPresentedCards(G, currentAction) as card, i}
+                  {#if currentAction === '3' && i === 2}
+                    <div class="h-[16.2vh] w-[11.53vh]" />
+                  {/if}
+                  {#if (i <= 1 && selectedPresentedIndex === '0') || (i >= 2 && selectedPresentedIndex === '1')}
+                    <button on:click={() => deselectFromPresented()} class="h-[16.2vh] w-[11.53vh]">
+                      <Card type="item" color={card.color} isSelected={true} isHoverable={true} isInverted={true} />
+                    </button>
+                  {:else}
+                    <button on:click={() => selectFromPresented(currentAction, i)} class="h-[16.2vh] w-[11.53vh]">
+                      <Card type="item" color={card.color} isSelected={false} isHoverable={true} />
+                    </button>
+                  {/if}
+                {/each}
+              </div>
             </div>
-          {/each}
+          {/if}
         {/if}
-      </div>
-    {/if}
-  {/if}
-</div>
+      {:else if opponentStage}
+        {#if opponentStage === 'draw'}
+          <Deck handleClick={() => false} isDisabled={true} />
+        {:else if opponentStage === 'selectCardsAsCurrentPlayer' || opponentStage === 'selectCardsAsOpposingPlayer'}
+          <div class="flex flex-row justify-center space-x-2">
+            {#if opponentStage === 'selectCardsAsCurrentPlayer'}
+              {#each Array(Number(currentAction) + 1) as _, i}
+                {#if currentAction === '3' && i === 2}
+                  <div class="h-[16.2vh] w-[11.53vh]" />
+                {/if}
+                <div class="h-[16.2vh] w-[11.53vh]">
+                  <Card type="empty" />
+                </div>
+              {/each}
+            {:else if opponentStage === 'selectCardsAsOpposingPlayer'}
+              {#each getPresentedCards(G, currentAction) as card, i}
+                {#if currentAction === '3' && i === 2}
+                  <div class="h-[16.2vh] w-[11.53vh]" />
+                {/if}
+                <div class="h-[16.2vh] w-[11.53vh]">
+                  <Card type="item" color={card.color} />
+                </div>
+              {/each}
+            {/if}
+          </div>
+        {/if}
+      {/if}
+    </div>
+    <div class="flex pt-10 justify-center gap-8">
+      {#if playerStage === 'selectCardsAsCurrentPlayer' && getIsActionUndoable(G, playerID)}
+        <button
+          on:click={() => undoAction()}
+          class="bg-gray-600 text-white text-xl h-14 w-32 rounded-full shadow-sm shadow-gray-600 hover:shadow hover:shadow-gray-600"
+        >
+          undo
+        </button>
+      {/if}
+      {#if playerStage === 'selectCardsAsCurrentPlayer' || playerStage === 'selectCardsAsOpposingPlayer'}
+        <button
+          on:click={() => confirmSelection(selectedCards, selectedPresentedIndex)}
+          disabled={getIsConfirmationButtonDisabled(selectedCards, selectedPresentedIndex)}
+          class="bg-violet-300 text-xl h-14 w-32 rounded-full shadow-sm shadow-gray-600 hover:shadow hover:shadow-gray-600 disabled:bg-gray-200 disabled:shadow-none"
+        >
+          confirm
+        </button>
+      {/if}
+    </div>
+  </section>
+{/if}
