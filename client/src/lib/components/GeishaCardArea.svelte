e4ae171b10abf432a98e3f9ac47797ac11dbb84e
Jeo D <jeo@dfor.dev>
2023-01-13 18:16:16 -0700

    Fix misnamed prop

--- client/src/lib/components/GeishaCardArea.svelte
+++ client/src/lib/components/GeishaCardArea.svelte
@@ -24,7 +24,7 @@
       <CardStack
         color={geishaCard.color}
         count={geishaCard.playerItemCards[opponentID].length}
-        isFlipped={true}
+        isUpsideDown={true}
       />
     {/each}
   </div>
