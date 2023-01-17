d34580738dc8c19fe6405ba0d889a6fdc4c6f2e8
Jeo D <jeo@dfor.dev>
2023-01-16 21:15:11 -0700

    Add missing prop

--- client/src/lib/components/Board.svelte
+++ client/src/lib/components/Board.svelte
@@ -141,7 +141,7 @@
 
 {#if G && ctx}
   <main class="grid grid-cols-[2fr_3fr] grid-rows-[1fr_4fr_1fr] gap-2 h-screen p-2 font-nunito bg-purple-100">
-    <Opponent player={getPlayer(G, opponentID)} />
+    <Opponent player={getPlayer(G, opponentID)} {hasGameStarted} />
     <SharedInterface
       {G}
       {hasGameStarted}
