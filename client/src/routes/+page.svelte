5c744df7cdbd28bee736734f0824b4c2d09a5b3b
Jeo D <jeo@dfor.dev>
2023-01-19 23:28:32 -0700

    Format with Prettier

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -32,7 +32,10 @@
 
 <main class="bg-purple-100 font-nunito text-black/[.87] flex flex-col items-center gap-10 pt-10 pb-60">
   <h1 class="text-4xl">Welcome to Hanamikoji</h1>
-  <p class="max-w-prose">In Hanamikoji, two players compete to earn the favor of seven Geisha by collected the performance items the Geisha desire.</p>
+  <p class="max-w-prose">
+    In Hanamikoji, two players compete to earn the favor of seven Geisha by collected the performance items the Geisha
+    desire.
+  </p>
   <button
     on:click={() => startMatch()}
     disabled={isLoading}
@@ -48,9 +51,15 @@
   <section class="max-w-prose pb-2">
     <h3 class="text-2xl">Summary:</h3>
     <div class="flex flex-col gap-2 pt-1">
-      <p>The object of the game is to win the favor of 4 Geisha or to have 11 Charm Points. (There is a maximum of 7 Geisha and 21 Charm Points.)</p>
+      <p>
+        The object of the game is to win the favor of 4 Geisha or to have 11 Charm Points. (There is a maximum of 7
+        Geisha and 21 Charm Points.)
+      </p>
       <p>You and your opponent take turns performing actions in order to play Item Cards.</p>
-      <p>At the end of a round, for each Geisha, if you have more Item Cards of the corresponding type than your opponent, you win the Geisha's favor and gain Charm Points equal to the number on the Geisha Card.</p>
+      <p>
+        At the end of a round, for each Geisha, if you have more Item Cards of the corresponding type than your
+        opponent, you win the Geisha's favor and gain Charm Points equal to the number on the Geisha Card.
+      </p>
       <p>The game continues until one player achieves a winning score at the end of a round.</p>
     </div>
   </section>
@@ -61,15 +70,24 @@
     <p>The number 3 on the Geisha Card means if you win this Geisha's favor, you get 3 Charm Points.</p>
     <p>Item Card</p>
     <Card type="item" color={Color.BLUE} />
-    <p>This is the blue Geisha Card and the corresponding blue Item Card. The Item Cards for each Geisha share the Geisha's color and number.</p>
-    <p>The number on the Item Card also represents how many of that card are in the deck e.g. there are 3 blue Item Cards in the deck.</p>
+    <p>
+      This is the blue Geisha Card and the corresponding blue Item Card. The Item Cards for each Geisha share the
+      Geisha's color and number.
+    </p>
+    <p>
+      The number on the Item Card also represents how many of that card are in the deck e.g. there are 3 blue Item Cards
+      in the deck.
+    </p>
   </section>
   <section class="max-w-prose pb-2">
     <h3 class="text-2xl">Game Setup:</h3>
     <div class="flex flex-col gap-2 pt-1">
       <p>7 Geisha Cards in the center of the board</p>
       <p>1 Victory Marker on the center of each Geisha Card</p>
-      <p>A deck of 20 Item Cards (There are 21 Item Cards in total, but one Item Card is removed from the deck at random at the beginning of each round.)</p>
+      <p>
+        A deck of 20 Item Cards (There are 21 Item Cards in total, but one Item Card is removed from the deck at random
+        at the beginning of each round.)
+      </p>
       <p>A hand of 6 Item Cards for each player dealt from the deck</p>
       <p>4 Action Markers for each player</p>
     </div>
@@ -124,18 +142,33 @@
     <h3 class="text-2xl">Scoring:</h3>
     <div class="flex flex-col gap-2 pt-1">
       <p>After both players have performed 4 actions, each player reveals and plays their hidden card from Action 1.</p>
-      <p>Then for each Geisha, compare the number of Item Cards each player has played on their side of the board. If one player has more Item Cards, move the Victory Marker for that Geisha to that player's side.</p>
-      <p>A Victory Marker on a player's side means that that player wins the Geisha and gains Charm Points equal to the number on the Geisha Card.</p>
-      <p>After determining the position of each Victory Marker, calculate the number of Geisha and Charm Points each player has.</p>
+      <p>
+        Then for each Geisha, compare the number of Item Cards each player has played on their side of the board. If one
+        player has more Item Cards, move the Victory Marker for that Geisha to that player's side.
+      </p>
+      <p>
+        A Victory Marker on a player's side means that that player wins the Geisha and gains Charm Points equal to the
+        number on the Geisha Card.
+      </p>
+      <p>
+        After determining the position of each Victory Marker, calculate the number of Geisha and Charm Points each
+        player has.
+      </p>
       <p>If no player has won, reset the deck and start another round.</p>
-      <p>Keep in mind that Victory Markers remain in place, so during the next round's scoring phase, a player will win a Geisha if the Victory Marker is already on that player's side and the number of Item Cards is tied.</p>
+      <p>
+        Keep in mind that Victory Markers remain in place, so during the next round's scoring phase, a player will win a
+        Geisha if the Victory Marker is already on that player's side and the number of Item Cards is tied.
+      </p>
     </div>
   </section>
   <section class="max-w-prose pb-2">
     <h3 class="text-2xl">Winning the Game:</h3>
     <div class="flex flex-col gap-2 pt-1">
       <p>If any player wins 4 Geisha or has 11 or more Charm Points, that player wins the game.</p>
-      <p>If one player wins 4 Geisha and the other has 11 or more Charm Points, the player with 11 or more Charm Points wins the game.</p>
+      <p>
+        If one player wins 4 Geisha and the other has 11 or more Charm Points, the player with 11 or more Charm Points
+        wins the game.
+      </p>
     </div>
   </section>
 </main>
