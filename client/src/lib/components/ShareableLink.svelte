bee280b05cef389f3d2f9b3aaee25735f5dc6f18
Jeo D <jeo@dfor.dev>
2023-01-20 21:53:19 -0700

    Adjust shareable link for 1280px and up

--- client/src/lib/components/ShareableLink.svelte
+++ client/src/lib/components/ShareableLink.svelte
@@ -19,18 +19,18 @@
 </script>
 
 <div class="place-self-center max-w-prose h-full">
-  <p class="text-3xl my-6">Share this link with a friend to start the game.</p>
-  <p class="text-3xl my-6 font-mono bg-gray-200 px-4 py-2 rounded-xl flex justify-between">
+  <p class="my-3 xl:text-xl 2xl:text-2xl fhd:text-3xl fhd:my-6">Share this link with a friend to start the game.</p>
+  <p class="bg-gray-200 font-mono flex justify-between px-3 py-1 rounded-lg xl:text-xl xl:my-3 2xl:text-2xl 2xl:px-4 2xl:my-4 fhd:text-3xl fhd:py-2 fhd:my-6 fhd:rounded-xl">
     {baseURL + matchID}
     <button
       on:click={() => copyLink(baseURL + matchID)}
       disabled={isCopied}
-      class="bg-gray-300 hover:bg-gray-400 grid inline h-10 rounded-xl p-2"
+      class="bg-gray-300 hover:bg-gray-400 grid inline p-2 rounded-md fhd:rounded-xl fhd:h-10 fhd:p-2"
     >
       {#if !isCopied}
-        <img src="/images/content-copy.svg" alt="copy" class="h-6 inline place-items-center" />
+        <img src="/images/content-copy.svg" alt="copy" class="inline place-items-center h-4 fhd:h-6" />
       {:else}
-        <img src="/images/check.svg" alt="copied" class="h-6 inline place-items-center" />
+        <img src="/images/check.svg" alt="copied" class="inline place-items-center h-4 fhd:h-6" />
       {/if}
     </button>
   </p>
