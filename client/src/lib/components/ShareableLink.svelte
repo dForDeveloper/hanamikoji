2c733efb902eb38180d03aab262c97cf1df1390a
Jeo D <jeo@dfor.dev>
2023-01-24 00:03:44 -0700

    Fix shareable link styling on mobile

--- client/src/lib/components/ShareableLink.svelte
+++ client/src/lib/components/ShareableLink.svelte
@@ -20,7 +20,7 @@
 
 <div class="place-self-center max-w-prose h-full">
   <p class="message-text">Share this link with a friend to start the game.</p>
-  <p class="bg-gray-200 font-mono flex justify-between py-1 rounded-lg lg:text-base lg:px-1 lg:leading-8 xl:text-xl xl:px-2 xl:my-3 xl:leading-8 2xl:text-2xl 2xl:px-3 2xl:my-4 fhd:text-3xl fhd:py-2 fhd:my-6 fhd:rounded-xl">
+  <p class="bg-gray-200 font-mono flex justify-between py-1 rounded-lg text-base px-1 leading-7 lg:leading-8 xl:text-xl xl:px-2 xl:my-3 xl:leading-8 2xl:text-2xl 2xl:px-3 2xl:my-4 fhd:text-3xl fhd:py-2 fhd:my-6 fhd:rounded-xl">
     {baseURL + matchID}
     <button
       on:click={() => copyLink(baseURL + matchID)}
