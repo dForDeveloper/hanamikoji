f8e39ca5e5022c18e4e400efbd0d38648401ccbb
Jeo D <jeo@dfor.dev>
2023-01-28 20:13:07 -0700

    Add missing api url env variable

--- client/src/routes/match/[matchID]/+page.ts
+++ client/src/routes/match/[matchID]/+page.ts
@@ -1,12 +1,13 @@
 import { error } from '@sveltejs/kit';
 import type { LoadEvent } from '@sveltejs/kit';
 import type { LobbyAPI } from 'boardgame.io';
+import { PUBLIC_BASE_API_URL } from '$env/static/public';
 
 export async function load(loadEvent: LoadEvent) {
   const fetchMatchById = async () => {
     try {
       const { matchID } = loadEvent.params;
-      const res = await loadEvent.fetch(`http://localhost:8000/games/hanamikoji/${matchID}`);
+      const res = await loadEvent.fetch(`${PUBLIC_BASE_API_URL}/games/hanamikoji/${matchID}`);
       const match: LobbyAPI.Match = await res.json();
       return match;
     } catch (err) {
