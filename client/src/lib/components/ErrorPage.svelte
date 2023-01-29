e44d9d3b35c885fbc72b1a893d2740c3b9b1eeee
Jeo D <jeo@dfor.dev>
2023-01-29 23:26:30 +0000

    style: #29 Adjust UI on iPhone and MacBook (#35)

--- client/src/lib/components/ErrorPage.svelte
+++ client/src/lib/components/ErrorPage.svelte
@@ -3,7 +3,7 @@
   export let errorMessage: string;
 </script>
 
-<main class="h-screen bg-purple-100 font-nunito text-black/[.87] flex flex-col items-center gap-10 pt-10">
+<main class="h-screen font-nunito text-black/[.87] flex flex-col items-center gap-10 pt-10">
   <h1 class="text-4xl">{status}: {errorMessage}</h1>
   <a href="/" class="text-2xl underline">return to main page</a>
 </main>
