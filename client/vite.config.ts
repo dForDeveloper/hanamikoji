6832cf824f97d1b1ffdb1fbe6becae5dd847cb2f
Jeo D <jeo@dfor.dev>
2023-01-31 17:52:37 -0700

    Move client tests to a different directory

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import type { UserConfig } from 'vite';
 const config: UserConfig = {
   plugins: [sveltekit()],
   test: {
-    include: ['src/**/*.test.ts'],
+    include: ['tests/*.test.ts'],
     environment: 'jsdom',
   },
 };
