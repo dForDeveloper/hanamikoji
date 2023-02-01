8853c6c034372a17e27a3c8f489fccba22f7000c
Jeo D <jeo@dfor.dev>
2023-01-31 17:25:34 -0700

    Set up Vitest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import type { UserConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
-const config = {
+const config: UserConfig = {
   plugins: [sveltekit()],
+  test: {
+    include: ['src/**/*.test.ts'],
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests.ts',
+    globals: true,
+  },
 };
 
 export default config;
